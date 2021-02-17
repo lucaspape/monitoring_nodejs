e7513c8a33b0196f8c143f51fad7b6dbfb0ff851
Lucas Pape <lucas.pape@web.de>
2021-02-17 14:02:00 +0000

    Integer

--- src/notification/send_notification_influxdb.js
+++ src/notification/send_notification_influxdb.js
@@ -8,6 +8,7 @@ module.exports = function(config, notify, host, check_command, state, message, s
         fields: {
           state: Influx.FieldType.STRING,
           message: Influx.FieldType.STRING,
+          out_int: Influx.FieldType.INTEGER,
           stdout: Influx.FieldType.STRING
         },
         tags: [
@@ -17,11 +18,17 @@ module.exports = function(config, notify, host, check_command, state, message, s
     ]
   });
 
+  var out_int = 0;
+
+  if(!isNaN(stdout)){
+    out_int = stdout;
+  }
+
   influxdb.writePoints([
     {
       measurement: check_command.command_name,
       tags: { host: host.name },
-      fields: { state:state, message:message, stdout:stdout }
+      fields: { state:state, message:message, out_int:out_int, stdout:stdout }
     }
   ]).catch(err => {
     console.log('Could not save to influxdb');
